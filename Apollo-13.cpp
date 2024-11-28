710b3de6ad78b9675e07217b8a78982487c62985
DashellF <djf1517@gmail.com>
2024-11-28 12:12:16 -0800

    Create Apollo-13.cpp

---
+++ Apollo-13.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+void getOutput();
+
+int main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n;
+        cin >> n;
+        cin.ignore();
+
+        for (int i = 0; i < n; i++) {
+                for (int i = 0; i < testCases; i++){
+                          string input = "", x = "", y = "", z = "";
+                          int xI, yI, zI = 0;
+                          int k = 0;
+                          getline(cin, input);
+                          while(input.at(k) != ' '){
+                              x += input.substr(k, k+1);
+                              k++;
+                          }
+                          k++;
+                          while(input.at(k) != ' '){
+                              y += input.substr(k, k+1);
+                              k++;
+                          }
+                          k++;
+                          z = input.substr(k);
+                          xI = stoi(x);
+                          yI = stoi(y);
+                          zI = stoi(z);
+                                     
+                          xI += 180;
+                          yI += 180;
+                          zI += 180;
+                          if (xI > 360){
+                                  xI -= 360;
+                          }
+                          if (yI > 360){
+                                  yI -= 360;
+                          }
+                          if (zI > 360){
+                                  zI -= 360;
+                          }
+                          cout << xI << " " << yI << " " << zI;
+                }
+        }
+
+        return 0;
+}
+
