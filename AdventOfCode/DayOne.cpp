c6005907be43071f8f0b8c040877ddaacf78e5d1
DashellF <djf1517@gmail.com>
2024-12-07 03:12:41 +0000

    added files

---
+++ AdventOfCode/DayOne.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    for(int testcase = 0; testcase < testCases; testcase++){
+    }
+}
---
+++ CodeQuest/UnderPressure.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    for(int testcase = 0; testcase < testCases; testcase++){
+        int c, l, diff;
+    }
+}
