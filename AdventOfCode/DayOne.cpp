258eaab32f0c5c3adec13eb99efd7b7a089b98bc
DashellF <djf1517@gmail.com>
2024-12-12 21:10:50 +0000

    modified:   AdventOfCode/DayOne.cpp

--- AdventOfCode/DayOne.cpp
+++ AdventOfCode/DayOne.cpp
@@ -5,13 +5,40 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int arr[1000];
+    int arrT[1000];
     ifstream input("AdventOfCode/DayOne.txt");
     string inputS;
     while (getline(input, inputS)){
-        int one, two;
+        int one, two, i = 0;
         istringstream line(inputS);
-        // if (line >> one >> two){
-            
-        // }
+        if (line >> one >> two){
+            arr[i] = one;
+            arrT[i] = two;
+            cout << arr[i] << "\n";
+            cout << arrT[i] << "\n";
+            i++;
+        }
     }
+    int sub;
+    for (int i = 0; i < 1000 - 1; i++){
+        for (int k = 0; k < 1000 - 1; k++){
+            if (arr[k] > arr[k+1]){
+                sub = arr[k];
+                arr[k] = arr[k+1];
+                arr[k+1] = sub;
+            }
+            if (arrT[k] > arrT[k+1]){
+                sub = arrT[k];
+                arrT[k] = arr[k+1];
+                arrT[k+1] = sub;
+            }
+        }
+    }
+    cout << "sorted!" << "\n";
+    int output = 0;
+    for (int i = 0; i < 1000; i++){
+        output += abs(arr[i] - arrT[i]);
+    }
+    cout << output << "\n";
 }
