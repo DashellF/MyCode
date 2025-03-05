19a3057b6708683d1efca9c2dffae769246bc9e8
DashellF <djf1517@gmail.com>
2025-03-05 03:15:28 +0000

    modified:   CodeQuest/prob25 	modified:   CodeQuest/prob25.cpp

--- CodeQuest/prob25.cpp
+++ CodeQuest/prob25.cpp
@@ -20,28 +20,56 @@ int main() {
             string left = solve(line.substr(0,line.find("=")));
             string right = line.substr(line.find("=")+1);
             if (stod(left) == stod(right)){
-                cout << "VALID" << "\n";
+                cout << "Correct" << "\n";
             }
             else {
-                cout << left << "\n";
+                string out = left;
+                for (int i = out.length()-1; i > -1; i--){
+                    if (out[i] == '0' || out[i] == '.'){
+                        out = out.substr(0,i);
+                    }
+                    else {
+                        break;
+                    }
+                }
+                if (out.length() < left.length() - 3){
+                    cout << out << "\n";
+                }
+                else {
+                    cout << fixed << setprecision(3) << stod(left) << "\n";
+                }
             }
         }
         return 0;
 }
 
 string solve(string eq){
+    // cout << eq << "\n";
     if (eq.find("(") != string::npos){
         int parenIndex;
-        for (int i = 0; i < eq.length(); i++){
-            if (eq[i] == ')'){
+        int depth = 1;
+        for (int i = eq.find("(")+1; i < eq.length(); i++){
+            if (eq[i] == '('){
+                depth++;
+            }
+            else if (eq[i] == ')'){
+                depth--;
+            }
+            if (depth == 0){
                 parenIndex = i;
+                break;
             }
         }
-        return solve(eq.substr(0,eq.find("(")) + solve(eq.substr(eq.find("("), parenIndex - eq.find("(") - 1)) + eq.substr(parenIndex + 1));
+        return solve(eq.substr(0,eq.find("(")) + solve(eq.substr(eq.find("(")+1, parenIndex - eq.find("(") - 1)) + eq.substr(parenIndex + 1));
     }
     else if (eq.find("SQRT") != string::npos){
         pair<double, int> AnumAfter = numAfter(eq.substr(eq.find("SQRT") + 4));
-        return solve(eq.substr(0,eq.find("SQRT")) + to_string((sqrt(AnumAfter.first))) + eq.substr(eq.find("SQRT") + 4 + AnumAfter.second));
+        if (eq.find("-SQRT") == 0){
+            return solve(eq.substr(0,eq.find("-SQRT")) + to_string((-1 * sqrt(AnumAfter.first))) + eq.substr(eq.find("SQRT") + 4 + AnumAfter.second));
+        }
+        else {
+            return solve(eq.substr(0,eq.find("SQRT")) + to_string((sqrt(AnumAfter.first))) + eq.substr(eq.find("SQRT") + 4 + AnumAfter.second));
+        }
     }
     else if (eq.find("^") != string::npos){
         pair<double, int> AnumBefore = numBefore(eq.substr(0,eq.find("^")));
@@ -66,7 +94,7 @@ string solve(string eq){
     else if (eq.find("-") != string::npos && eq.find("-") != 0){
         pair<double, int> AnumBefore = numBefore(eq.substr(0,eq.find("-")));
         pair<double, int> AnumAfter = numAfter(eq.substr(eq.find("-") + 1));
-        return solve(eq.substr(0,eq.find("-") - AnumBefore.second) + to_string((AnumBefore.first - AnumAfter.first)) + eq.substr(eq.find("-") + 1 + AnumAfter.second + 1));
+        return solve(eq.substr(0,eq.find("-") - AnumBefore.second) + to_string((AnumBefore.first - AnumAfter.first)) + eq.substr(eq.find("-") + 1 + AnumAfter.second));
     }
     else {
         return eq;
